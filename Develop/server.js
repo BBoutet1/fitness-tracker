f7a1565678f9a74f2f0db97b1073b0d4ff367ab6
Brice Boutet <boutetlb@gmail.com>
2020-07-05 17:02:04 -0400

    mongoose configured and connection to mongo database

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,13 +10,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-//     useNewUrlParser: true,
-//     useFindAndModify: false
-// });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log("Connected To Mongo DataBase");
+}).catch((err) => {
+    console.log("DataBase Connection Error " + err);
+});
 
 // routes
-app.use(require("./routes/api.js"));
+// app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
