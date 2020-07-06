7014afe6640f62ca07e4319df1f0dc1cd28fea0f
Brice Boutet <boutetlb@gmail.com>
2020-07-06 01:46:51 -0400

    Repo directory reorganized

---
+++ models/workout.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter an exercise type"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter the exercise name"
+        },
+        duration: {
+            type: Number,
+            trim: true,
+            required: "Enter the duration in minutes"
+        },
+        weight: {
+            type: Number,
+            trim: true
+        },
+        reps: {
+            type: Number,
+            trim: true
+        },
+        sets: {
+            type: Number,
+            trim: true
+        },
+        distance: {
+            type: Number,
+            trim: true
+        }
+    }]
+});
+
+const Workouts = mongoose.model("Workouts", workoutSchema);
+
+module.exports = Workouts;
