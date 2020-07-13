18c2fdc253de5fe25e252458ff4341a2d76031a7
Brice Boutet <boutetlb@gmail.com>
2020-07-12 22:34:12 -0400

    Seeds connected database indexed

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,7 +2,7 @@ let mongoose = require("mongoose");
 let db = require("../models/workout");
 
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useFindAndModify: false
